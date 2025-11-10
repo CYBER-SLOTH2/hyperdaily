910980eaf16a73c6f66b8ec80a5b79ac36e06399
CYBER-SLOTH2 <suresh9test@gmail.com>
2025-11-10 15:48:32 +0530

    a

---
+++ src/consts.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "HiFi Studio And Mobile";
+export const SITE_DESCRIPTION = "Your ultimate source for HiFi audio, mobile gadgets, reviews, tips, and tech news";
+export const SITE_KEYWORDS = 'HiFi, Mobile, Gadgets, Reviews, Tech News, Audio, Smartphones';
