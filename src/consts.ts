1642b41c4c321544878707f868e56bef9d1fc257
CYBER-SLOTH2 <suresh9test@gmail.com>
2025-11-10 16:33:44 +0530

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "HiFi Studio And Mobile";
-export const SITE_DESCRIPTION = "Your ultimate source for HiFi audio, mobile gadgets, reviews, tips, and tech news";
-export const SITE_KEYWORDS = 'HiFi, Mobile, Gadgets, Reviews, Tech News, Audio, Smartphones';
+export const SITE_TITLE = "Hyperdaily";
+export const SITE_DESCRIPTION = "Daily trending blogs covering internet culture, tech, lifestyle, business, entertainment and what's buzzing today.";
+export const SITE_KEYWORDS = "trending news, viral stories, daily buzz, tech trends, internet culture, entertainment updates, lifestyle trends, Hyperdaily";
